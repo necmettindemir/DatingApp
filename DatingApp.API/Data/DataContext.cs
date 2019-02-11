846ae107302ddb41b887b7605cc8e99d297b70f1
Necmettin Demir <necmettindemir@yahoo.com>
2019-02-11 04:44:43 +0300

    init commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Models;
+
+
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {        
+        
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {}
+
+        public DbSet<Value> Values { get; set; }
+
+
+        // public async Task<List<PeopleType>> GetAsyncPeopleList()
+        // {
+        //     List<PeopleType> peopleList = new List<PeopleType>()
+        //     {
+        //     new PeopleType(){ Name = "Frank", Gender = "M" },
+        //     new PeopleType(){ Name = "Rose", Gender = "F" },
+        //     new PeopleType(){ Name = "Mark", Gender = "M" },
+        //     new PeopleType(){ Name = "Andrew", Gender = "M" },
+        //     new PeopleType(){ Name = "Sam", Gender = "M" },
+        //     new PeopleType(){ Name = "Lisa", Gender = "F" },
+        //     new PeopleType(){ Name = "Lucy", Gender = "F" }
+        //     };
+
+        //     var result = from e in peopleList
+        //                 where e.Gender == "M"
+        //                 select e;
+        //     return await result.AsQueryable().ToListAsync();
+        // }
+
+    }
+}
